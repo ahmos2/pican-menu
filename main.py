8e8f6826edc80433e72f89f3845bdc1e9460c255
Martin Sundhaug <martinsundhaug@gmail.com>
2014-11-19 12:49:11 +0100

    Load menu from menu.json

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ BCM_S2_SW_PIN=23
 
 import RPi.GPIO as GPIO
 from subprocess import call
-import time
+import time,json
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BCM_S1_SW_PIN, GPIO.IN)
@@ -11,6 +11,12 @@ GPIO.setup(BCM_S2_SW_PIN, GPIO.IN)
 
 menuposition=0
 awaitNullInput=1
+
+menu=0
+with open('menu.json','r') as menuFile:
+	menu=json.load(menuFile)
+print "Items: ",len(menu)
+
 
 while True:
 	S1_SW=GPIO.input(BCM_S1_SW_PIN)
@@ -21,8 +27,13 @@ while True:
 			awaitNullInput=0
 	else:
 		if S1_SW==0:
-			call(["espeak",str(menuposition)])
+			print menu[menuposition].get("command")
+			call(menu[menuposition].get("command"))
 		elif S2_SW==0:
 			menuposition+=1
+			if(menuposition>=len(menu)):
+				print "EPOSMAX: ",menuposition
+				menuposition=0
 			print(menuposition)
+			call(["espeak",menu[menuposition].get("name")])
 	time.sleep(0.2)
